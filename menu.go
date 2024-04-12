ea424fcdfca16faa97d9c9b37abe5b844d1e330f
Jason <jason@zgwit.com>
2024-04-13 00:09:15 +0800

    小改动

--- menu.go
+++ menu.go
@@ -5,6 +5,7 @@ import "github.com/zgwit/iot-master/v4/menu"
 func init() {
 	menu.Register("camera", &menu.Menu{
 		Name:       "视频监控",
+		Icon:       "video-camera",
 		Domain:     []string{"admin"},
 		Privileges: nil,
 		Items: []*menu.Item{
